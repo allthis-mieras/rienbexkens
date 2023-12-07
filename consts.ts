7e81106b3a978bd6c7d77c9ac341cca7f2acf425
mieras <mieras@gmail.com>
2023-12-07 12:56:55 +0100

    basehead meta shizzle

---
+++ consts.ts
@@ -0,0 +1,11 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Rien Bexkens / Rini Tortellini'
+export const SITE_DESCRIPTION =
+	'Rien Bexkens is a Rotterdam-based director, writer and producer, author of feature films, shorts, documentaries and commercials.'
+export const NAME_REAL = 'Rien Bexkens'
+export const NAME = 'Rini Tortellini'
+export const TERMS = ''
+export const KVK = ''
+export const IBAN = ''
