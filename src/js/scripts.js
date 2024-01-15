cabd3f2ccdcbc7e792f591791fa9843a77b20aa7
mieras <mieras@gmail.com>
2024-01-15 15:59:58 +0100

    allemaal dingen gedaan

---
+++ src/js/scripts.js
@@ -0,0 +1,11 @@
+var options = {
+	animate: true,
+	patternWidth: 436.85,
+	patternHeight: 485.48,
+	grainOpacity: 0.2,
+	grainDensity: 10,
+	grainWidth: 1,
+	grainHeight: 1
+}
+
+grained('#grain', options)
