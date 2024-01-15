a328e7c28ae7ae9284cc1bdf8892d2901f4945ff
mieras <mieras@gmail.com>
2024-01-15 17:04:46 +0100

    kleurtjes

--- consts.ts
+++ consts.ts
@@ -1,9 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Rien Bexkens / Rini Tortellini'
+export const SITE_TITLE = 'Rien Bexkens / Rini Tortellini '
 export const SITE_DESCRIPTION =
-	'Rien Bexkens is a Rotterdam-based director, writer and producer, author of feature films, shorts, documentaries and commercials.'
+	'Rien Bexkens is a Rotterdam-based director, writer and producer of feature films, shorts, documentaries and commercials.'
 export const NAME_REAL = 'Rien Bexkens'
 export const NAME = 'Rini Tortellini'
 export const TERMS = ''
